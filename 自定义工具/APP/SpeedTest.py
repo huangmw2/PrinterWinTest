0b54d996be70906711e827be12b98a612612da59
anshao <1226233917@qq.com>
2024-10-12 17:59:49 +0800

    修改打印速度测试

--- 自定义工具/APP/SpeedTest.py
+++ 自定义工具/APP/SpeedTest.py
@@ -1,9 +1,21 @@
 # speed_test.py
 import tkinter as tk
 from tkinter import ttk
+import time
 
 class Speed_Test:
     def __init__(self, parent):   
+        self.is_running = False
+        self.start_time = 0  # 保存计时开始的时间
+        self.elapsed_time = 0  # 保存已经过去的时间
+        self.timer_id = None  # 保存 after() 的定时器 ID
+        self.Print_speed = 0
+        self.threeinches_path = ""
+        self.twoinches_path = ""
+        if __name__ == "__main__":
+            self.root = parent
+            self.root.title("Speed测试")
+            self.root.geometry("700x500+600+300")        
         #创建一个矩形区域
         self.frame = tk.Frame(parent, bd=2, relief=tk.GROOVE)
         self.frame.pack(fill=tk.BOTH, expand=True)
@@ -13,59 +25,58 @@ class Speed_Test:
         # 添加第二个Label控件
         self.label2 = tk.Label(self.frame, text="速度:000mm/s", font=("Helvetica", 32))
         self.label2.pack(padx=20, pady=(0, 10))  # 垂直方向的间距调整为20像素
-
-  
-        # 串口设置部分
-        self.label_com_port = tk.Label(self.frame, text="选择串口号:")
-        self.label_com_port.place(x=0,y=150)
-
-        self.com_port_entry = ttk.Combobox(self.frame, width=10, values=['COM1', 'COM2', 'COM3', 'COM4', 'COM5', 'COM6', 'COM7', 'COM8', 'COM9'])  # 根据实际情况修改串口号列表
-        self.com_port_entry.place(x=80,y=150)
-        self.com_port_entry.set('COM3')
-        self.label_baud_rate = tk.Label(self.frame, text="输入波特率:")
-        self.label_baud_rate.place(x=0,y=180)
-
-        self.baud_rate_entry = tk.Entry(self.frame, width=10)
-        self.baud_rate_entry.place(x=80,y=180)
-        self.baud_rate_entry.insert(0, "115200")  # 默认波特率为115200
-        #打印机数据选择
-        self.label_speed = tk.Label(self.frame, text="打印速度设置：")
-        self.label_speed.place(x=0,y=210)
-        self.speep_entry = ttk.Combobox(self.frame, width=10, values=['1', '2', '3', '4', '5', '6', '7', '8'])  # 根据实际情况修改串口号列表
-        self.speep_entry.place(x=80,y=210)
-        self.speep_entry.set('8')
-        self.speed_set_button = tk.Button(self.frame, text="设置", width=10, command=self.Print_speed_set)
-        self.speed_set_button.place(x=180,y=210)  # 垂直方向上部分间距为20像素
-
         # 发送选项部分
+        self.label_Timertype = tk.Label(self.frame, text="计时方式：",font=("仿宋",12,"bold"))
+        self.label_Timertype.place(x=10,y=150)       
         self.send_option = tk.StringVar()
         self.send_option.set("start_timer")
-
-        self.radio_send_now = tk.Radiobutton(self.frame, text="发送马上计时", variable=self.send_option, value="start_timer")
-        self.radio_send_now.place(x=200,y=150)
-        self.radio_send_after = tk.Radiobutton(self.frame, text="发送完后开始计时", variable=self.send_option, value="start_after_send")
-        self.radio_send_after.place(x=200,y=180)
-
+        self.radio_send_now = tk.Radiobutton(self.frame, text="发送马上计时", variable=self.send_option, value="start_timer",font=("仿宋",12))
+        self.radio_send_now.place(x=90,y=150)
+        self.radio_send_after = tk.Radiobutton(self.frame, text="发送完后开始计时", variable=self.send_option, value="start_after_send",font=("仿宋",12))
+        self.radio_send_after.place(x=220,y=150)
         #2寸和3寸选择
+        self.label_Paperinches = tk.Label(self.frame, text="纸张尺寸：",font=("仿宋",12,"bold"))
+        self.label_Paperinches.place(x=10,y=180)  
         self.size_option = tk.StringVar()
         self.size_option.set("two_inches")
-		
-        self.radio_Two_Inches = tk.Radiobutton(self.frame, text="2寸", variable=self.size_option, value="two_inches")
-        self.radio_Two_Inches.place(x=330,y=150)
-        self.radio_Three_Inches = tk.Radiobutton(self.frame, text="3寸", variable=self.size_option, value="three_inches")
-        self.radio_Three_Inches.place(x=330,y=180)
+        self.radio_Two_Inches = tk.Radiobutton(self.frame, text="2寸", variable=self.size_option, value="two_inches",font=("仿宋",12),command=self.update_paper_length)
+        self.radio_Two_Inches.place(x=90,y=180)
+        self.radio_Three_Inches = tk.Radiobutton(self.frame, text="3寸", variable=self.size_option, value="three_inches",font=("仿宋",12),command=self.update_paper_length)
+        self.radio_Three_Inches.place(x=220,y=180)
+        #纸张的长度
+        self.label_Paperlength  = tk.Label(self.frame, text="纸张长度：",font=("仿宋",12,"bold"))
+        self.label_Paperlength.place(x=10,y=210)   
+        self.Paperlength_entry = tk.Entry(self.frame,width=10)
+        self.Paperlength_entry.place(x=100,y=210) 
+        if self.size_option.get() == "two_inches":
+            self.Paperlength_entry.insert(0,"1280") 
+        else :
+            self.Paperlength_entry.insert(0,"1600") 
+          
+        self.label_mmunit = tk.Label(self.frame, text="MM",font=("仿宋",12))
+        self.label_mmunit.place(x=180,y=210)     
+        self.LengthSet_button = tk.Button(self.frame, text="长度设置", width=10, command=self.SetPaperLength,font=("仿宋",12))
+        self.LengthSet_button.place(x=210,y=210)  
+        #打印机数据选择
+        self.label_speed = tk.Label(self.frame, text="打印速度：",font=("仿宋",12,"bold"))
+        self.label_speed.place(x=10,y=240)
+        self.speep_entry = ttk.Combobox(self.frame, width=10, values=['1', '2', '3', '4', '5', '6', '7', '8'])  # 根据实际情况修改串口号列表
+        self.speep_entry.place(x=100,y=240)
+        self.speep_entry.set('8')
+        self.speed_set_button = tk.Button(self.frame, text="速度设置", width=10, command=self.Print_speed_set,font=("仿宋",12))
+        self.speed_set_button.place(x=210,y=240)  # 垂直方向上部分间距为20像素
+
 		
         # 开始和复位按钮
-        self.start_stop_button = tk.Button(self.frame, text="开始", width=10, command=self.start_stop_timer)
-        self.start_stop_button.place(x=0,y=250)  # 垂直方向上部分间距为20像素
+        self.start_stop_button = tk.Button(self.frame, text="开始", width=10, command=self.start_stop_timer,font=("仿宋",12),bg="green")
+        self.start_stop_button.place(x=0,y=280)  
 		
-
-        self.reset_button = tk.Button(self.frame, text="复位", width=10, command=self.reset_timer)
-        self.reset_button.place(x=90,y=250)  # 垂直方向上部分间距为20像素
+        self.reset_button = tk.Button(self.frame, text="复位", width=10, command=self.reset_timer,font=("仿宋",12))
+        self.reset_button.place(x=120,y=280)  
 
         # 强制结束按钮
-        self.confirm_button = tk.Button(self.frame, text="保存", width=10, command=self.save_data)
-        self.confirm_button.place(x=180,y=250) 
+        self.confirm_button = tk.Button(self.frame, text="保存", width=10, command=self.save_data,font=("仿宋",12))
+        self.confirm_button.place(x=240,y=280) 
 		#清空文件
         #self.clear_data()
 
@@ -73,10 +84,70 @@ class Speed_Test:
         print("test")
 
     def start_stop_timer(self):
-        print("test")
+        if not self.is_running:
+            # 开始计时
+            self.is_running = True
+            self.start_stop_button.config(text="停止", bg="red")
+            self.start_time = time.time() - self.elapsed_time  # 记录开始时间，考虑已经过去的时间
+            self.update_timer()  # 启动计时器
+            self.start_stop_button.config(text="停止", bg="red")
+            # 在这里添加计时功能代码
+        else:
+            # 停止计时
+            self.is_running = False
+            if self.timer_id:
+                self.frame.after_cancel(self.timer_id)  # 取消计时
+            self.start_stop_button.config(text="开始", bg="green")
+            Paperlength = int(self.Paperlength_entry.get())
+            PrintTime = int(self.elapsed_time*10)
+            self.Print_speed = Paperlength*10/PrintTime
+            speed_format = f"速度:{int(self.Print_speed):03d}mm/s"
+            self.label2.config(text=speed_format)
+            # 在这里添加停止计时功能代码
+
+    def update_timer(self):
+        # 计算已经过去的时间
+        self.elapsed_time = time.time() - self.start_time
+        minutes, seconds = divmod(self.elapsed_time, 60)
+        milliseconds = int((self.elapsed_time - int(self.elapsed_time)) * 10) #取出第一位小数
+        time_format = f"时间:{int(minutes):02}:{int(seconds):02}.{milliseconds}"
+        self.label.config(text=time_format)
+        # 如果计时器在运行，继续更新
+        if self.is_running:
+            self.timer_id = self.frame.after(100, self.update_timer)  # 每 100 毫秒更新一次
 
     def reset_timer(self):
-        print("test")
+        self.is_running = False
+        self.elapsed_time = 0
+        self.label.config(text="时间:00:00.0")
+        self.label2.config(text="速度:000mm/s")
+        self.start_stop_button.config(text="开始", bg="green")
+        if self.timer_id:
+            self.frame.after_cancel(self.timer_id)
         
     def save_data(self):
         print("test")
+
+    def SetPaperLength(self):
+        Paperlength = int(self.Paperlength_entry.get())
+        PrintTime = int(self.elapsed_time*10)
+        self.Print_speed = Paperlength*10/PrintTime
+        speed_format = f"速度:{int(self.Print_speed):03d}mm/s"
+        self.label2.config(text=speed_format)
+
+    def update_paper_length(self):
+        inches = self.size_option.get()
+        if inches == "two_inches":
+            self.Paperlength_entry.delete(0, tk.END)
+            self.Paperlength_entry.insert(0,"1280")
+        elif inches == "three_inches":
+            self.Paperlength_entry.delete(0, tk.END)
+            self.Paperlength_entry.insert(0,"1600")
+        else :
+            print("纸张长度选择错误")
+        pass
+if __name__ == "__main__":
+
+    root = tk.Tk()
+    app = Speed_Test(root)
+    root.mainloop()
