598cf7a3deb2d997db6ef220bbd301740dfda0e4
anshao <1226233917@qq.com>
2024-10-31 17:55:21 +0800

    新增一个config.json，修改RAM.PY

--- 自定义工具/APP/RAMTest.py
+++ 自定义工具/APP/RAMTest.py
@@ -6,3 +6,12 @@ class RAM_Test:
     def __init__(self,parent):
         self.frame = tk.Frame(parent,bd=2,relief=tk.GROOVE)
         self.frame.pack(fill=tk.BOTH,expand=True)
+        if __name__ == "__main__":
+            self.root = parent
+            self.root.title("RAM测试/设置")
+            self.root.geometry("700x500+600+300") 
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = RAM_Test(root)
+    root.mainloop()
